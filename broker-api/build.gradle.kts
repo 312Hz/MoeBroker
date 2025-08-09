5f53837f886f6fbb579ea0cc8093e3e14f7400df
hanhan2001 <764932129@qq.com>
2025-08-09 10:54:45 +0800

    build: 整理 gradle 内容

--- broker-api/build.gradle.kts
+++ broker-api/build.gradle.kts
@@ -1,19 +1,3 @@
-plugins {
-    id("java")
-}
-
-group = "me.xiaoying.moebroker"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.yaml:snakeyaml:2.4")
 }
