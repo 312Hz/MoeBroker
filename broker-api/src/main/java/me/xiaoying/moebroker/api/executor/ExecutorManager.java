f682087a08790451f38ae27872fa313cf96a8521
hanhan2001 <764932129@qq.com>
2025-08-14 21:43:43 +0800

    feat: ExecutorManager

---
+++ broker-api/src/main/java/me/xiaoying/moebroker/api/executor/ExecutorManager.java
@@ -0,0 +1,35 @@
+package me.xiaoying.moebroker.api.executor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorManager {
+    private static final Map<String, ExecutorService> executors = new HashMap<>();
+
+    // 创建默认线程池
+    static {
+        ExecutorManager.getFixedExecutor("broker", Runtime.getRuntime().availableProcessors());
+    }
+
+    public static ExecutorService getExecutor(final String name) {
+        return ExecutorManager.executors.get(name);
+    }
+
+    public static ExecutorService getFixedExecutor(String name, int size) {
+        return ExecutorManager.executors.computeIfAbsent(name, k -> Executors.newFixedThreadPool(size));
+    }
+
+    public static ExecutorService getSingleExecutor(String name) {
+        return ExecutorManager.executors.computeIfAbsent(name, k -> Executors.newSingleThreadExecutor());
+    }
+
+    public static ExecutorService getCacheExecutor(String name) {
+        return ExecutorManager.executors.computeIfAbsent(name, k -> Executors.newCachedThreadPool());
+    }
+
+    public static ExecutorService getScheduledExecutor(String name, int size) {
+        return ExecutorManager.executors.computeIfAbsent(name, k -> Executors.newScheduledThreadPool(size));
+    }
+}
